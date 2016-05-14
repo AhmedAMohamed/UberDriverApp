c2e964261ee9f9c7fb96b89e23cf5b016ffac4d3
Ahmed Alaa <ahmedalaa3307023@gmail.com>
2016-05-15 00:30:06 +0200

    Main activity commit
    Signed-off-by: Ahmed Alaa <ahmedalaa3307023@gmail.com>

--- app/src/main/java/com/example/ahmeda/uberdriverapp/MainActivity.java
+++ app/src/main/java/com/example/ahmeda/uberdriverapp/MainActivity.java
@@ -1,104 +1,230 @@
 package com.example.ahmeda.uberdriverapp;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMarkerDragListener, LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.Buffer;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMarkerDragListener, com.google.android.gms.location.LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    public final int  MY_PERMISSIONS_REQUEST_FINE_LOCATION = 0;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private LatLng myLocation;
+    private LocationRequest request;
+    private GoogleMap mMap;
+    private LatLng currentLocation;
+    private LatLng requestLocation;
+
+    BroadcastReceiver driverLocationUpdatesReceiver;
+
+    Button signUp;
+    Button login;
+    Button requestRide;
+    private double lat;
+    private double longitude;
+    private MarkerOptions markerOption;
+    private Marker currentLocationMarker;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        markerOption = new MarkerOptions();
 
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        boolean Services_available = checkGooglePlayServices(this);
+        if (Services_available) {
+            buildGoogleApiClient();
+            createLocationRequest();
         }
+        currentLocation = new LatLng(0,0);
+        requestLocation = new LatLng(10,10);
 
-        return super.onOptionsItemSelected(item);
+        //signUp = (Button)findViewById(R.id.signupButton);
+        //login = (Button)findViewById(R.id.login);
+        //requestRide = (Button)findViewById(R.id.requestRideBtn);
     }
 
     @Override
-    public void onLocationChanged(Location location) {
+    public void onStart() {
+        super.onStart();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
+    }
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we
+     * just add a marker near Africa.
+     */
+    @Override
+    public void onMapReady(GoogleMap map) {
 
+        mMap = map;
+
+        currentLocationMarker = mMap.addMarker(new MarkerOptions().position(new LatLng(0,0)));
+
+        mMap.addMarker(new MarkerOptions()
+                .position(requestLocation)
+                .draggable(true)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+
+        // mMap.addMarker(new MarkerOptions().position(mLastLocation).title("My Actual Location"));
+        //mMap.setMyLocationEnabled(true);
 
     }
 
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
 
     @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-
-    }
-
-    @Override
-    public void onConnected(Bundle bundle) {
-
+    public void onConnected(@Nullable Bundle bundle) {
+        startLocationUpdates();
     }
 
     @Override
     public void onConnectionSuspended(int i) {
 
     }
-
+    //This method is invoked after requestLocationUpdates
     @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
+    public void onLocationChanged(Location location) {
+        mLastLocation = location;
+        //currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        if (mLastLocation != null) {
+            // Toast.makeText(this, "Latitude:" + mLastLocation.getLatitude() + ", Longitude:" + mLastLocation.getLongitude(), Toast.LENGTH_LONG).show();
+            mLastLocation = location;
+            if (mLastLocation != null) {
+                Toast.makeText(this, "Latitude:" + mLastLocation.getLatitude() + ", Longitude:" + mLastLocation.getLongitude(), Toast.LENGTH_LONG).show();
+                currentLocationMarker.setPosition(new LatLng(location.getLatitude(), location.getLongitude()));
+            }
+            double lastLat = 0;
+            int latEqual = Double.compare(mLastLocation.getLatitude(), lastLat);
+            double lastLong = 0;
+            int LongEqual = Double.compare(mLastLocation.getLongitude(), lastLong);
+            if ((mLastLocation.getLatitude() - lastLat == 0.00005) && (mLastLocation.getLongitude() - lastLong == 0.00005)) {
+                lastLat = mLastLocation.getLatitude();
+                lastLong = mLastLocation.getLongitude();
+                this.lat = mLastLocation.getLatitude();
+                this.longitude = mLastLocation.getLongitude();
+                PutLocation test = new PutLocation();
+                test.execute();
+            }
+        }
+    }
+
+    //Create Location requests to periodically request a location update
+    protected void createLocationRequest() {
+        request = new LocationRequest();
+        request.setInterval(20000);
+        request.setFastestInterval(5000);
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    public synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
 
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
+    protected void startLocationUpdates() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
 
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+        }else{
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, request, this);
+            mMap.setMyLocationEnabled(true);
+        }
     }
 
     @Override
-    public void onMarkerDragStart(Marker marker) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case MY_PERMISSIONS_REQUEST_FINE_LOCATION:{
+                //The application granted the permission
+                startLocationUpdates();
+            }
 
+        }
     }
+
+
+    public boolean checkGooglePlayServices(Context mContext){
+        GoogleApiAvailability googleAPI = GoogleApiAvailability.getInstance();
+        int result = googleAPI.isGooglePlayServicesAvailable(this);
+        AlertDialog unavailable_play_services=new AlertDialog.Builder(this).create();
+        unavailable_play_services.setTitle("Google Play services are not installed");
+        unavailable_play_services.setMessage("Please Install Google Play Services");
+        if(result != ConnectionResult.SUCCESS) {
+            if(googleAPI.isUserResolvableError(result)) {
+                unavailable_play_services.show();
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+
 
     @Override
     public void onMarkerDrag(Marker marker) {
@@ -107,6 +233,63 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onMarkerDragEnd(Marker marker) {
+        requestLocation = marker.getPosition();
+    }
 
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    class PutLocation extends AsyncTask<Void,Void,Void> {
+        private static final String ID = "id";
+        String Url="http://uberlikeapp-ad3rhy2.rhcloud.com//api/user/updateUserLocation";
+        StringBuilder stringBuilder;
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            Toast.makeText(getBaseContext(),stringBuilder,Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try{
+                URL url = new URL(Url);
+                HttpURLConnection urlConnection =(HttpURLConnection)url.openConnection();
+                urlConnection.setRequestMethod("PUT");
+                String type = "driver";
+                urlConnection.setRequestProperty("type", type);
+                urlConnection.setRequestProperty("driver_id", getSharedPreferences("Uber", 0).getString("driver_id", null));
+                urlConnection.setRequestProperty("lat", String.valueOf(lat));
+                urlConnection.setRequestProperty("lng", String.valueOf(longitude));
+                urlConnection.setDoInput(true);
+                urlConnection.setDoOutput(true);
+                urlConnection.connect();
+                InputStream in=urlConnection.getInputStream();
+                stringBuilder = new StringBuilder();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
+                }
+            }
+            catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally{}
+            return null;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(driverLocationUpdatesReceiver);
     }
 }
